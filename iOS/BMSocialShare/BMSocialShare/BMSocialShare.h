2517d7612808ad45ab77370eae5d19a9e4be5f5c
Vinzenz Weber <vinzenz@blockhaus-medien.at>
2011-11-04 10:32:59 +0100

    initial commit

---
+++ iOS/BMSocialShare/BMSocialShare/BMSocialShare.h
@@ -0,0 +1,13 @@
+//
+//  BMSocialShare.h
+//  BMSocialShare
+//
+//  Created by Vinzenz-Emanuel Weber on 04.11.11.
+//  Copyright (c) 2011 Blockhaus Medienagentur. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BMSocialShare : NSObject
+
+@end
---
+++ iOS/BMSocialShare/BMSocialShare/BMSocialShare.m
@@ -0,0 +1,23 @@
+//
+//  BMSocialShare.m
+//  BMSocialShare
+//
+//  Created by Vinzenz-Emanuel Weber on 04.11.11.
+//  Copyright (c) 2011 Blockhaus Medienagentur. All rights reserved.
+//
+
+#import "BMSocialShare.h"
+
+@implementation BMSocialShare
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
